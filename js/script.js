d3780cf29fe96010767c7e7f26535a1703a4aeac
Jagoda Wawrzynek <jagoda.wawrzynek@gmail.com>
2020-07-28 20:05:25 +0200

    Modified script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var computerMove, playerMovie;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMovie = 'papier';
-printMessage('Zagrałem' + playerMovie + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
