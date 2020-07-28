62e391676c19c588efb0d06430886db681b11a46
Jagoda Wawrzynek <jagoda.wawrzynek@gmail.com>
2020-07-28 19:03:07 +0200

    Add script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMovie;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMovie = 'papier';
+printMessage('Zagrałem' + playerMovie + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
