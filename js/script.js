957f3cf19c0a42c635d8a77709a7e87a5d3ee168
Jagoda Wawrzynek <jagoda.wawrzynek@gmail.com>
2020-07-30 21:53:47 +0200

    Modified code script.js and remove buttons to index.html

--- js/script.js
+++ js/script.js
@@ -1,66 +1,49 @@
-var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest;
-var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber, x;
+var argMoveNumber;
 
-buttonTest = document.getElementById('button-test');
-buttonRock = document.getElementById('button-rock');
-buttonPaper = document.getElementById('button-paper');
-buttonScissors = document.getElementById('button-scissors');
 
 /**
  * Describe this function...
  */
-function buttonClicked(argButtonName) {
-  clearMessages();
-  console.log(argButtonName + ' został kliknięty');
-}
 
-/**
- * Describe this function...
- */
-function getMoveName(argMoveId) {
-  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-  if (argMoveId == 1) {
+function getMoveName(argMoveNumber) {
+    if (argMoveNumber == 1) {
     return 'kamień';
-  } else if (argMoveId == 2) {
+  } else if (argMoveNumber == 2) {
     return 'papier';
-  } else if (argMoveId == 3) {
+  } else if (argMoveNumber == 3) {
     return 'nożyce';
   } else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
-    return 'kamień';
+    printMessage('Nie znam ruchu o id ' + argMoveNumber + '.');
+    return 'nieznany ruch';
   }
 }
-
-/**
- * Describe this function...
- */
-function displayResult(argPlayerMove, argComputerMove) {
-  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-    printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
-    printMessage('Wygrywasz!');
-  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-    printMessage('Wygrywasz!');
-  } else if (argPlayerMove == argComputerMove) {
-    printMessage('Remis');
-  } else {
-    printMessage('Przegrywasz :(');
-  }
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-}
-playerMove = argButtonName;
-//playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('wybór ruchu gracza to: ' + playerInput);
-//playerMove = getMoveName(playerInput);
-console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
 
-buttonTest.addEventListener('click', function(){ buttonClicked('Guzik TEST'); });
-buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
-buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
-buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
+let computerMove = getMoveName(randomNumber);
+console.log('Ruch komputera to: ' + computerMove);
+printMessage('Ruch komputera to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = getMoveName(playerInput);
+console.log('Ruch gracza to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerMove)
+
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień'){
+  printMessage('Remis. Zagraj jeszcze raz');
+} else if (computerMove == 'papier' && playerMove == 'papier'){
+  printMessage('Remis. Zagraj jeszcze raz');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis. Zagraj jeszcze raz');
+} else {
+  printMessage('Przegrałeś. Spróbuj jeszcze raz.')
+}
